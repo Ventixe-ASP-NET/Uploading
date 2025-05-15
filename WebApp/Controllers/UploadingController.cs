94d9823e3460b11a34e4c1fa273516868d5346fd
Luiz Oliveira <luiz.oliveira@gronalund.com>
2025-05-15 07:26:20 +0200

    Create UploadingController

---
+++ WebApp/Controllers/UploadingController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Handlers;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadingController : ControllerBase
+    {
+        private readonly IFileHandler _fileHandler;
+
+        public UploadingController(IFileHandler fileHandler)
+        {
+            _fileHandler = fileHandler;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("File is not provided or is empty.");
+            }
+
+            try
+            {
+                var imageFileUri = await _fileHandler.UploadFileAsync(file);
+                return Ok(new { ImageFileUri = imageFileUri });
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (if logging is set up)
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Error = ex.Message });
+            }
+        }
+    }
+}
+
